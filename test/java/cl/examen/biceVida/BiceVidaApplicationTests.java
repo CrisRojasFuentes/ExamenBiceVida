fcc15d0e900abd47d44501aa17b68da80f979945
CrisRojasFuentes <rojas.fuentes.cris@gmail.com>
2024-10-13 16:11:32 -0300

    primer Commit

---
+++ test/java/cl/examen/biceVida/BiceVidaApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.examen.biceVida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiceVidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
