6877c511dbf75be92b98c7be7f3dd665e786da60
CrisRojasFuentes <rojas.fuentes.cris@gmail.com>
2024-10-21 11:15:54 -0300

    Ordenado

---
+++ main/java/cl/examen/biceVida/models/Cliente.java
@@ -0,0 +1,50 @@
+package cl.examen.biceVida.models;
+
+public class Cliente {
+		private int id;
+		private String rut;
+		private String name;
+
+		public Cliente() {
+			super();
+		}
+
+		public Cliente(int id, String rut, String name) {
+			this.id = id;
+			this.rut = rut;
+			this.name = name;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getRut() {
+			return rut;
+		}
+
+		public void setRut(String rut) {
+			this.rut = rut;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		@Override
+		public String toString() {
+			return "Cliente [id=" + id + ", rut=" + rut + ", name=" + name + "]";
+		}
+
+	}
+
+
+
